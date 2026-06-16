13d9f589f67aa7dca46162ae4217684e0a7c452c
lastrioktaviyani-cell <lastrioktaviyani@gmail.com>
2026-06-17 00:34:16 +0700

    Update prisma.ts

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,19 +1,17 @@
 import { PrismaClient } from "@prisma/client";
 
 const globalForPrisma = globalThis as unknown as {
-  prisma: PrismaClient | undefined;
+prisma: PrismaClient | undefined;
 };
 
 export const prisma =
-  globalForPrisma.prisma ??
-  new PrismaClient({
-    log: ["error"],
-  });
+globalForPrisma.prisma ||
+new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
+globalForPrisma.prisma = prisma;
 }
 
 export function getDb() {
-  return prisma;
+return prisma;
 }
